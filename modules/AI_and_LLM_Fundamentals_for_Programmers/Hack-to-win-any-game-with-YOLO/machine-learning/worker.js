8c1d08594435632028a669ffffd4f94c73293fe7
Iris Costa <irisdevf14@gmail.com>
2026-07-14 16:49:27 -0300

    feat: implement YOLOv5 model loading and inference pipeline in Web Worker

--- modules/AI_and_LLM_Fundamentals_for_Programmers/Hack-to-win-any-game-with-YOLO/machine-learning/worker.js
+++ modules/AI_and_LLM_Fundamentals_for_Programmers/Hack-to-win-any-game-with-YOLO/machine-learning/worker.js
@@ -2,12 +2,78 @@ importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest');
 
 const MODEL_PATH = `yolov5n_web_model/model.json`;
 const LABELS_PATH = `yolov5n_web_model/labels.json`;
+const INPUT_MODEL_DIMETIONS = 640;
 
 
+let _labels = [];
+let _model = null;
+async function loadModelAndLabels(){
+    await tf.ready();
+
+    _labels = await ( await fetch(LABELS_PATH)).json();
+    _model = await tf.loadGraphModel(MODEL_PATH);
+
+    // warm up the model with a dummy input
+    const dummyInput = tf.ones(_model.inputs[0].shape);
+    await _model.executeAsync(dummyInput);
+    tf.dispose(dummyInput);
+
+    postMessage({type: 'model-loaded'})
+    
+} 
+
+function preprocessImage(input){
+    return tf.tidy(() => {
+        const image = tf.browser.fromPixels(input);
+
+        return tf.image
+        .resizeBilinear(image, [INPUT_MODEL_DIMETIONS, INPUT_MODEL_DIMETIONS])
+        .div(255)
+        .expandDims(0)
+    })
+} 
+
+async function runInference(tensor){
+    const output = await _model.executeAsync(tensor);
+    tf.dispose(tensor);
+
+    /* 
+    Assume que as primeiras 3 saídas do modelo são:
+    1. boxes: [1, num_boxes, 4] - coordenadas das caixas delimitadoras
+    2. scores: [1, num_boxes, num_classes] - pontuações de confiança para cada classe
+    3. classes: [1, num_boxes] - índices das classes previstas
+    */
+    const [boxes, scores, classes] = output.slice(0, 3)
+    const [boxesData, scoresData, classesData] = await Promise.all(
+        [
+            boxes.data(),
+            scores.data(),
+            classes.data(),
+        ]
+    )
+
+
+    output.forEach(t => t.dispose());
+
+    return {
+        boxes: boxesData,
+        scores: scoresData,
+        classes: classesData
+    }
+}
+
+loadModelAndLabels()
 
 self.onmessage = async ({ data }) => {
     if (data.type !== 'predict') return
+    if (!_model) return 
 
+    const input =  preprocessImage(data.image);
+    const {width, height} = data.image
+
+    const inferenceResult = await runInference(input);
+    debugger;
+    
     postMessage({
         type: 'prediction',
         x: 400,
